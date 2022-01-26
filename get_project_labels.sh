2be0fd8b13bf7c3090f148b16f253aa8391ecad9
Didier Gourillon <dgourillon@google.com>
2022-01-26 14:43:22 +0100

    after overall cleanup

--- get_project_labels.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-PROJECT_NUMBER=$1
-PROJECT_RESOURCE_FILE=/home/dgourillon/jq_explorer/resources/cloudresourcemanager.googleapis.com/Project
-
-grep $PROJECT_NUMBER $PROJECT_RESOURCE_FILE |  while read -r cluster_line ; do
-
-done
--- hierarchy.sh
+++ hierarchy.sh
@@ -1,36 +1,29 @@
-#!/bin/bash
+#!/bin/sh
 
-RESOURCES_FOLDER=/home/dgourillon/jq_explorer/resources
+INPUT_FOLDER="output/raw_resources"
+OUTPUT_FILE="output/hierarchy.txt"
 
-get_folder_name(){
-	if [[ $1 == *"folder"* ]]; then
-		grep "\"name\":\"\/\/cloudresourcemanager.googleapis.com/$1" $RESOURCES_FOLDER/cloudresourcemanager.googleapis.com/Folder | jq '.resource["data"]["displayName"]' | tr -d '"' | tr -d '\n'
-	fi
-	if [[ $1 == *"organization"* ]]; then
-	grep "\"name\":\"\/\/cloudresourcemanager.googleapis.com/$1" $RESOURCES_FOLDER/cloudresourcemanager.googleapis.com/Organization | jq '.resource["data"]["displayName"]' | tr -d '"' | tr -d '\n'
-	fi
-	#if [[ $1 == *"project"* ]]; then
-	#grep "\"name\":\"\/\/cloudresourcemanager.googleapis.com/$1" $RESOURCES_FOLDER/cloudresourcemanager.googleapis.com/Project | jq '.resource["data"]["name"]' | tr -d '"' | tr -d '\n'
-	#fi
+jq '. | "\(.resource["data"]["name"]) \(.resource["data"]["displayName"]) "'   $INPUT_FOLDER/cloudresourcemanager.googleapis.com/Folder > folder_map
+jq '. | "\(.name) \(.resource["data"]["projectId"]) "' $INPUT_FOLDER/cloudresourcemanager.googleapis.com/Project > project_map
+jq '. | "\(.name) \(.resource["data"]["displayName"]) "' $INPUT_FOLDER/cloudresourcemanager.googleapis.com/Organization  > org_map
+echo " Org_name / Folder 1 / Folder 2 / .... / Project ID" > $OUTPUT_FILE
 
-}
+for current_line in $(jq '. | "\(.resource["data"]["name"])_\(.ancestors) " ' $INPUT_FOLDER/cloudresourcemanager.googleapis.com/Project | sed -E 's/\"|\[|\]|\\//g' ); do
+    project=$(echo $current_line | awk -F '_' '{print $1}')
+    ancestors=$(echo $current_line | awk -F '_' '{print $2}')
+    ancestors_names=""
+    for str in ${ancestors//,/ } ; do 
 
-analyze_file_ancestors(){
-	file_to_analyse=$1
-	cat $1 | while read -r line ; do
-	    #echo "Processing $project_line"
-	    	name=$(echo $line | jq '.name')
-		ancestors=$(echo $line | jq '.ancestors')
-		ancestors_line_result="$name,"
-		ancestors_list=$(echo $line | jq '.ancestors' | grep -v '\[\|]' | tr -d '" ,')
-		while read -r ancestor_line ; do
-			ancestor_line_name=$(get_folder_name $ancestor_line)
-			ancestors_line_result=$(echo "$ancestors_line_result,$ancestor_line,$ancestor_line_name")
-		done <<<$(echo -e "$ancestors_list") 
-		echo "$ancestors_line_result" |  awk -F ',' '{for(i=NF;i>=1;i--) printf "%s,", $i;print ""}'
-	done
+        to_add=""
+        [[ $str =~ ^.*folder.*$ ]] && to_add=$(grep $str  folder_map | awk '{print $2}')
+        [[ $str =~ ^.*project.*$ ]] && to_add=$(grep $str  project_map | awk '{print $2}')
+        [[ $str =~ ^.*org.*$ ]] && to_add=$(grep $str  org_map | awk '{print $2}')
+        ancestors_names="$to_add / $ancestors_names"
+    done
+    echo "$ancestors_names" >> $OUTPUT_FILE
+done
 
-}
-
-analyze_file_ancestors $1  
-
+rm folder_map
+rm project_map
+rm org_map
+cat $OUTPUT_FILE | sort
